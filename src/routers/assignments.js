4d17b520fdd4d4d230bd2495abf4123688a9cb29
coreyjkbartels <cbartels@eagles.bridgewater.edu>
2026-02-13 14:16:49 -0500

    Add support for personal states of assignments for users

--- src/routers/assignments.js
+++ src/routers/assignments.js
@@ -3,6 +3,7 @@ import auth from '../middleware/auth.js'
 import Assignment from '../models/assignment.js'
 import { isCourse, isCourseMember, isCourseModerator } from '../middleware/courseAccess.js'
 import { isAssignment } from '../middleware/assignmentAccess.js'
+import AssignmentUserState from '../models/assignmentUserState.js'
 
 
 const router = new Router()
@@ -17,7 +18,7 @@ router.post('/courses/:courseId/assignments',
             createdBy: user._id,
             course: course._id,
             title: body.title,
-            source: courseMembership.role == 'member' ? 'manuel' : 'moderator',
+            source: courseMembership.role == 'member' ? 'manual' : 'moderator',
             dueAt: body.dueAt
         }
 
@@ -167,6 +168,106 @@ router.delete('/courses/:courseId/assignments/:assignmentId',
             console.log(error)
 
             res.status(400).send(error)
+        }
+    })
+
+//Get Assignments with User States
+router.get('/courses/:courseId/my/assignments',
+    auth, isCourse, isCourseMember,
+    async (req, res) => {
+        const { course, user, query } = req
+
+        const filter = {
+            course: course._id,
+            status: 'active'
+        }
+
+        if (query?.status) {
+            filter.status = query.status
+        }
+
+        if (query?.status == 'all') {
+            delete filter.status
+        }
+
+        try {
+            const assignments = await Assignment.aggregate([
+                { $match: filter },
+                {
+                    $lookup: {
+                        from: 'assignmentuserstates',
+                        localField: '_id',
+                        foreignField: 'assignment',
+                        as: 'userState'
+                    }
+                },
+                {
+                    $unwind: {
+                        path: '$userState',
+                        preserveNullAndEmptyArrays: true
+                    }
+                },
+                {
+                    $project: {
+                        __v: 0,
+                        updatedAt: 0,
+                        'userState._id': 0,
+                        'userState.assignment': 0,
+                        'userState.course': 0,
+                        'userState.user': 0,
+                        'userState.createdAt': 0,
+                        'userState.updatedAt': 0,
+                        'userState.__v': 0
+                    }
+                }
+
+            ])
+
+
+            res.status(200).send(assignments)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(error)
+        }
+    })
+
+//Edit my version of assignment
+router.patch('/courses/:courseId/assignments/:assignmentId/my-state',
+    auth, isCourse, isCourseMember, isAssignment,
+    async (req, res) => {
+        const { body: mods, course, user, assignment } = req
+
+        let userState = await AssignmentUserState.findOne({ assignment: assignment._id, user: user._id })
+
+        if (!userState) {
+            userState = await AssignmentUserState.create({
+                assignment: assignment._id,
+                user: user._id,
+                course: course._id
+            })
+        }
+
+        if (!mods) {
+            return res.status(200).send(userState)
+        }
+
+        const props = Object.keys(mods)
+        const modifiable = ['state', 'personalNotes', 'personalDueAt', 'completedAt']
+
+        const isValid = props.every((prop) => modifiable.includes(prop))
+
+        if (!isValid) {
+            return res.status(400).send('Invalid updates.')
+        }
+
+        props.forEach((prop) => userState[prop] = mods[prop])
+
+        try {
+            await userState.save()
+            return res.status(200).send(userState)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json(error)
         }
     })
 export default router
